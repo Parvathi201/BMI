0ce4858a03160dbd6ab1d4e6c01d3ae9c92a8321
Parvathi Pradeep <118892822+Parvathi201@users.noreply.github.com>
2025-06-30 14:36:55 +0530

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,23 @@
+import streamlit as st
+st.title("BMI Calculator")
+with st.form("BMI Calculator"):
+    col1,col2,col3=st.columns([2,2,1])
+with col1:
+    weight= st.number_input("enter your weight in KGs")
+with col2:
+    height= st.number_input("enter your height in Meters")
+with col3:
+    submit=st.form_submit_button("Calulate")
+if submit:
+    BMI=round((weight / (height**2)),2)
+    if (BMI <= 18.5):
+        st.error("Underweight")
+    elif BMI>18.5 and BMI<=24.9:
+        st.success("Healthy / Normal")
+    elif BMI>=25 and BMI<=29.9:
+        st.warning("Overweight")
+    elif BMI>=30.0:
+        st.error("Obese")
+
+#C:\Users\hp\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\LocalCache\local-packages\Python312\Scripts\streamlit.exe run c:/Users/hp/Desktop/Python/bmi.py
+#running command
